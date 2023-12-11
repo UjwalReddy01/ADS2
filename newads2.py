410202973af7b49ca5e8a9f40881acf930de087f
UjwalReddy01 <149112372+UjwalReddy01@users.noreply.github.com>
2023-12-11 21:44:17 +0000

    Importing python libraries

---
+++ newads2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 12:57:46 2023
+
+@author: HP
+"""
+
+# Importing pandas package to read the file, for deriving pandas features and stats properties
+import pandas as pd
+# Importing numpy to read statistical properties
+import numpy as np
+# Importing matplotlib package for data plotting and visualization
+import matplotlib.pyplot as plt
+# Importing scipy functions to calculate skewness and kurtosis
+import scipy.stats as st
+
+# Defining a function to produce two dataframes, one with countries as columns and one with years as columns
+def read_csv_and_transpose(input_file, selected_countries):
+    data = pd.read_csv(input_file)
+    # Replacing the null values with zeroes using fillna() method
+    filled_data = data.fillna(0)
+    selected_data = filled_data[filled_data['Country Name'].isin(selected_countries)]
+    df_countries = pd.DataFrame(selected_data)
+    print(df_countries)
+    # Transposing data to derive a dataframe with years as columns
+    transpose = pd.DataFrame.transpose(selected_data)
+    header = transpose.iloc[0].values.tolist()
+    transpose.columns = header
+    transpose = transpose.iloc[1:]
+    df_years = transpose
+    print(transpose)
+    return df_countries, df_years
+
+
